a3d17655c49e891d05ec3158ff37f9a9ddb43320
yohannyvs <yohannyvs@gmail.com>
2017-08-05 10:22:43 -0600

    getPelicula -funcional-metodo

--- Swing_CIne_Spring_Api/src/main/java/com/progra/mavenproject1/NewClass.java
+++ Swing_CIne_Spring_Api/src/main/java/com/progra/mavenproject1/NewClass.java
@@ -1,7 +1,9 @@
 
 package com.progra.mavenproject1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,31 +11,57 @@ import org.springframework.web.client.RestTemplate;
 
 public class NewClass 
 {
-    private static final String api = "http://localhost:8080/Cine_api_rest_spring/";
+    private static final String api = "http://localhost:53344/Cine_api_rest_spring/";
     public static RestTemplate restTemplate = new RestTemplate();
     
     public static void main(String args[])
     {
-        //hola();
+        hola();
         NewJFrame f = new NewJFrame();
         //NewJFrame f = new NewJFrame();
         f.show();
-        inicio("rt","890");
+        //inicio("rt","890");
     }
     
-    /*public static void hola() 
+    public static void hola() 
     {
-        ResponseEntity<prueba[]> response;
-        response = restTemplate.getForEntity(api, prueba[].class);
+        ResponseEntity<pelicula[]> response;
+        response = restTemplate.getForEntity(api+"pelicula/", pelicula[].class);
+        List<pelicula> list = new ArrayList<>();
 
         System.out.println();
         System.out.println("GET All StatusCode = " + response.getStatusCode());
         System.out.println("GET All Headers = " + response.getHeaders());
         System.out.println("GET Body (object list): ");
         
-        Arrays.asList(response.getBody()).forEach(p -> System.out.println("--> " + p.getNombre()));
+        //Arrays.asList(response.getBody()).forEach(p -> System.out.println("--> " + p.getNombre()));
         
-    }*/
+        Arrays.asList(response.getBody()).forEach(p -> list.add( new pelicula(
+                    p.getId_presentacion(),
+                    p.getNombre(),
+                    p.getCategoria(),
+                    p.getIdioma(),
+                    p.getHora(),
+                    p.getNum_sala()
+            )));
+        
+        System.out.println(list.get(0).getNombre());
+        System.out.println(list.get(0).getIdioma());
+        /*for(pelicula p : response.getBody())
+        {
+            list.add( new pelicula(
+                    p.getId_presentacion(),
+                    p.getNombre(),
+                    p.getCategoria(),
+                    p.getIdioma(),
+                    p.getHora(),
+                    p.getNum_sala()
+            ));
+            
+            System.out.println(list);
+        }*/
+        
+    }
     
     public static String inicio( String id, String pass) 
     {
